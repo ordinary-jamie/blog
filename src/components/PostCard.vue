7e79b178c10ece2b28007d06ee09361807c1c728
ordinary-jamie <jamie@ordinarylab.dev>
2023-01-31 07:58:05 +1100

    Improve responsiveness for homepage
    * Tags placed in same div as preview text (slot) to ensure they
    don't flex in separate columns
    * Year align with top of preview to account for wrapping
    * Date field fixed width of 16 to stop wrapping
    * Post title to reduce in font size for mobile

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -51,28 +51,32 @@ const routeTo = computed(() => {
             <div class="p-2 rounded -mx-2 hover:ring-sky-500 hover:bg-sky-50 group">
                 <div class="flex flex-nowrap">
                     <div class="flex items-end">
-                        <span class="font-mono text-sm text-slate-500 group-hover:text-sky-500 pb-1">@{{
+                        <span class="font-mono text-xs sm:text-sm text-slate-500 group-hover:text-sky-500 pb-1">@{{
                             section
                         }}/</span>
-                        <h2 class="text-slate-700 font-bold">{{ title }}</h2>
+                        <h2 class="text-slate-700 font-bold text-sm sm:text-lg">{{ title }}</h2>
                     </div>
                     <div class="grow"></div>
                     <div class="flex flex-col text-right">
-                        <span class="text-slate-500 text-sm group-hover:text-sky-600">{{ displayDtFields.date }}</span>
+                        <span class="w-16 text-slate-500 text-sm group-hover:text-sky-600">{{
+                            displayDtFields.date
+                        }}</span>
                     </div>
                 </div>
-                <div class="flex text-slate-400 font-thin text-sm group-hover:text-slate-800 pl-1 items-center">
-                    <slot />&thinsp;
-                    <template v-for="(tag, i) in tags">
-                        <span class="text-violet-400 group-hover:text-violet-600 text-xs font-mono">#{{
-                            tag
-                        }}
-                        </span>
-                        {{ i<tags.length - 1 ? '&thinsp;' : '' }} </template>
-                            <div class="w-32 flex-grow"></div>
-                            <span class="text-slate-500 text-sm group-hover:text-sky-600">{{
-                                displayDtFields.year
-                            }}</span>
+                <div class="flex text-slate-400 font-thin text-sm group-hover:text-slate-800 pl-1 items-top">
+                    <div>
+                        <slot />&thinsp;
+                        <template v-for=" (tag, i) in tags">
+                            <span class="text-violet-400 group-hover:text-violet-600 text-xs font-mono">#{{
+                                tag
+                            }}
+                            </span>
+                            {{ i<tags.length - 1 ? '&thinsp;' : '' }} </template>
+                    </div>
+                    <div class="w-8 flex-grow"></div>
+                    <span class="text-slate-500 text-sm group-hover:text-sky-600">{{
+                        displayDtFields.year
+                    }}</span>
                 </div>
             </div>
         </RouterLink>
